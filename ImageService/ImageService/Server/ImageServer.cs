bd513daf0e382e1f122d5d3070f903f8b4eec18e
natan <natanno4@gmail.com>
2018-03-29 20:10:53 +0300

    update ImageServer

--- ImageService/ImageService/Server/ImageServer.cs
+++ ImageService/ImageService/Server/ImageServer.cs
@@ -16,12 +16,31 @@ namespace ImageService.Server
         #region Members
         private IImageController m_controller;
         private ILoggingService m_logging;
+        
         #endregion
 
         #region Properties
         public event EventHandler<CommandRecievedEventArgs> CommandRecieved;          // The event that notifies about a new Command being recieved
         #endregion
 
-       
+        public ImageServer(IImageServiceModal modal, ILoggingService logging, string [] pathes)
+        {
+            this.m_controller = new ImageController(modal);
+            this.m_logging = logging;
+            foreach (string path in pathes)
+            {
+                this.createHandeler(path);
+            }
+
+        }
+
+        public void createHandeler(string path)
+        {
+            IDirectoryHandler handler = new DirectoryHandler(this.m_controller, this.m_logging);
+            this.CommandRecieved += handler.OnCommandRecieved;
+            
+        }
+
+
     }
 }
