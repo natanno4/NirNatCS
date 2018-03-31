fa9bce898ec2f92057b645da2c61adbc0e649193
natan <natanno4@gmail.com>
2018-03-31 12:33:29 +0300

    update DirectoryHandlers

--- ImageService/ImageService/Controller/Handlers/DirectoyHandler.cs
+++ ImageService/ImageService/Controller/Handlers/DirectoyHandler.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ImageService.Infrastructure;
 using ImageService.Infrastructure.Enums;
 using ImageService.Logging;
 using ImageService.Logging.Modal;
@@ -22,8 +21,58 @@ namespace ImageService.Controller.Handlers
         private string m_path;                              // The Path of directory
         #endregion
 
-        public event EventHandler<DirectoryCloseEventArgs> DirectoryClose;              // The Event That Notifies that the Directory is being closed
+        public event EventHandler<DirectoryCloseEventArgs> DirectoryClose;   // The Event That Notifies that the Directory is being closed
+        public event EventHandler<CommandRecievedEventArgs> createFile;
 
-		// Implement Here!
+        public DirectoyHandler(ImageController m_controller, ILoggingService m_logging) {
+            this.m_controller = m_controller;
+            this.m_logging = m_logging;
+            this.m_dirWatcher = new FileSystemWatcher();
+        }
+
+        public void StartHandleDirectory(string dirPath) {
+            this.m_path = dirPath;
+            this.m_dirWatcher.Path = dirPath;
+            this.m_dirWatcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.FileName |
+                    NotifyFilters.CreationTime | NotifyFilters.DirectoryName | NotifyFilters.LastWrite |
+                    NotifyFilters.FileName;
+            this.m_dirWatcher.Filter = "*.jpj";
+            this.m_dirWatcher.Filter = "*.png";
+            this.m_dirWatcher.Filter = "*.gif";
+            this.m_dirWatcher.Filter = "*.bmp";
+            this.m_dirWatcher.Created += new 
+
+            this.m_dirWatcher.EnableRaisingEvents = true;
+
+        }
+        public void OnCommandRecieved(object sender, CommandRecievedEventArgs e)
+        {
+            Boolean result = true;
+            string message;
+            if (e.RequestDirPath.Equals(this.m_path)) {
+               message = this.m_controller.ExecuteCommand(e.CommandID, e.Args, out result);
+               if (result)
+                {
+                    this.m_logging.Log(message, MessageTypeEnum.INFO);
+                } else
+                {
+                    this.m_logging.Log(message, MessageTypeEnum.FAIL);
+                }
+            }
+           
+        }
+
+        public void createNewFile(object sender, CommandRecievedEventArgs e)
+        {
+            e.CommandID = (int) CommandEnum.NewFileCommand;
+            this.OnCommandRecieved(this, e);
+        }
+
+        public void closeFile(object sender, DirectoryCloseEventArgs e)
+        {
+
+        } 
+
+        // Implement Here!
     }
 }
--- ImageService/ImageService/Server/ImageServer.cs
+++ ImageService/ImageService/Server/ImageServer.cs
@@ -29,15 +29,16 @@ namespace ImageService.Server
             this.m_logging = logging;
             foreach (string path in pathes)
             {
-                this.createHandeler(path);
+                this.createHandler(path);
             }
 
         }
 
-        public void createHandeler(string path)
+        public void createHandler(string path)
         {
-            IDirectoryHandler handler = new DirectoryHandler(this.m_controller, this.m_logging);
+            IDirectoryHandler handler = new DirectoyHandler(this.m_controller, this.m_logging);
             this.CommandRecieved += handler.OnCommandRecieved;
+            handler.StartHandleDirectory(path);
             
         }
 
