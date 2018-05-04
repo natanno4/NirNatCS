3635f6336a4b0dcacac8a05bdec763b8a2e21361
natanno4 <natanno4@gmail.com>
2018-05-04 14:23:00 +0300

    ImageServiceGui

---
+++ ImageService/ImageGui/ViewModel/SetingsVM.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Model;
+
+public class SettingsVM : ViewModal
+{
+    private Model model;
+	public SettingsVM (ISettingsModel modell)
+    {
+        this.model = modell;
+	}
+
+}
---
+++ ImageService/ImageGui/ViewModel/ViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel;
+
+public class ViewModel : INotifypropertyChanged 
+{
+    public event PropertyChangedEventHandler propertyChanged;
+
+    public void NotifyPropertyChanged (string propname)
+    {
+        this.propertyChanged?.Invoke(this, new PropertyChangedEventArgs(propname));
+    }
+
+}
