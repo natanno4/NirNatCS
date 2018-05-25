16a459ec9741154e82928378bc7e48256eefa4bd
natanno4 <natanno4@gmail.com>
2018-05-25 22:40:30 +0300

    add inf

---
+++ ImageService/Infrastructure/MsgCommand.cs
@@ -0,0 +1,35 @@
+﻿
+using Newtonsoft.Json.Linq;
+
+namespace Infrastructure
+
+{
+    public class MsgCommand
+    {
+        public int commandID { get; set; }
+        public string[] args { get; set; }
+       
+        public MsgCommand(int id, string[] arg)
+        {
+            this.commandID = id;
+            this.args = arg;
+        }
+
+       
+        public static MsgCommand FromJSON(string str)
+        {
+            JObject jObject = JObject.Parse(str);
+            int command = (int)jObject["CommandId"];
+            JArray arr = (JArray)jObject["Args"];
+            string[] commandArgs = arr.ToObject<string[]>();
+            return new MsgCommand(command, commandArgs);
+        }
+        public string ToJSON()
+        {
+            JObject jObject = new JObject();
+            jObject["CommandId"] = commandID;
+            jObject["Args"] = new JArray(args);
+            return jObject.ToString();
+        }
+    } 
+}
