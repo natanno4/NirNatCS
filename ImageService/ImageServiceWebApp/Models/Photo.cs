c06b9cfeba98daebb78d7981e165e5108d8769b2
niraz888 <niraz888@gmail.com>
2018-06-08 16:11:21 +0300

    add  Photo and PhohoList classes

---
+++ ImageService/ImageServiceWebApp/Models/Photo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace ImageServiceWebApp.Models
+{
+    public class Photo
+    {
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "PhotoPath")]
+        public string PhotoPath { get; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Year")]
+        public string Year { get; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Month")]
+        public string Month { get; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Name")]
+        public string Name { get; }
+
+        public Photo(string path, string thumbPath)
+        {
+            Name = Path.GetFileName(path);
+            Month = Path.GetDirectoryName(path);
+            Month = new DirectoryInfo(Month).Name;
+            Year = Path.GetDirectoryName(Path.GetDirectoryName(path));
+            Year = new DirectoryInfo(Year).Name;
+        }
+    }
+}
---
+++ ImageService/ImageServiceWebApp/Models/PhotosList.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace ImageServiceWebApp.Models
+{
+    public class PhotosList
+    {
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "PhotoPath")]
+        public string PhotoPath { get; set; }
+
+        [Required]
+        [Display(Name = "ListPhotos")]
+        private List<Photo> ListPhotos = new List<Photo>();
+
+      
+        public int NumOfPhotos()
+        {
+            return this.ListPhotos.Count;
+        }
+        public void RemovePhoto(Photo rem)
+        {
+            foreach (Photo p in ListPhotos)
+            {
+                if (rem.Name.Equals(p.Name))
+                {
+                    ListPhotos.Remove(rem);
+                    string regularPathToRemove = rem.PhotoPath;
+                    File.Delete(regularPathToRemove);
+                    //need to add path of thumbnails to remove
+                    break;
+                }
+            }
+        }
+    }
+}
